8458327d9f2bfd07bf40a214c62bce14bf6313c9
Suhan Acharya <44167922+suhanacharya@users.noreply.github.com>
2018-12-17 20:34:36 +0530

    Update prg1.c
    Added Documentation, fixed spelling errors, syntax errors, logical errors.

--- sem-1-2/PCD_LAB/program1/prg1.c
+++ sem-1-2/PCD_LAB/program1/prg1.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<stdlib.h> //for exit(0);
 void main()
 {
 int a[100][100],b[100][100],m[100][100],a1,a2,b1,b2,i,j,k,sum;
 printf("enter the number of rows and columns of matrix A \n");
-scanf("%d",&a1,a2);
+scanf("%d%d",&a1,&a2);
 printf("enter the number of rows and column of matrix B \n");
-scanf("%d",&b1,b2);
+scanf("%d%d",&b1,&b2);
 if(a2==b1)
 {
-printf("matrices can be maultiplied ");
+printf("matrices can be multiplied ");
 }
 else
 {
-peintf("matrices cannot be multiplied ");
+printf("matrices cannot be multiplied ");
 exit(0);
 }
-printf("enter the elements of matrix A");
+printf("enter the elements of matrix A"); //input matrix A
 for(i=0;i<a1;i++)
 {
 for(j=0;j<a2;j++)
@@ -24,7 +24,7 @@ for(j=0;j<a2;j++)
 scanf("%d",&a[i][j]);
 }
 }
-printf("enter the elements of matrix B");
+printf("enter the elements of matrix B"); //input matrix B
 for(i=0;i<b1;i++)
 {
 for(j=0;j<b2;j++)
@@ -32,8 +32,10 @@ for(j=0;j<b2;j++)
 scanf("%d",&b[i][j]);
 }
 }
+//Compute product Matrix
 for(i=0;i<a1;i++)
 {
+sum=0;
 for(j=0;j<b2;j++)
 {
 for(k=1;k<b1;k++)
@@ -41,10 +43,9 @@ for(k=1;k<b1;k++)
 sum=sum+a[i][k]*b[k][j];
 }
 m[i][j]=sum;
-sum=0;
 }
 }
-printf("matrices after multiplication \n");
+printf("matrices after multiplication \n"); //Display product matrix
 for("i=o;i<a1;i++)
 {
 for(j=o;j<b2;j++)
