d2e2eb168f3ba98437e9f978fe92444aa586a2c4
Nathaniel <nathanielmat2@gmail.com>
2019-05-25 19:25:29 +0530

    Added MP programs

---
+++ sem-4/computer_science/MP Lab/07-KEIL.c
@@ -0,0 +1,15 @@
+#include <lpc214x.h>
+void main (void)
+{
+    int a,b,c,d,e,f;
+    a=4;
+    b=5;
+    d=3;
+    e=6;
+    c=a+b;
+    f=e-d;
+    a=5;
+    b=4;
+    d=3;
+    e=2;
+}
---
+++ sem-4/computer_science/MP Lab/6B-ARM-Additon.asm
@@ -0,0 +1,12 @@
+    AREA Program, CODE, READONLY
+    ENTRY
+start
+    MOV R0, #0x40000000
+    LDR R1, [R0]
+    ADD R0, R0, #04
+    LDR R2, [R0]
+    ADDS R1, R1, R2
+    ADD R0, R0, #04
+    STR R1, [R0]
+SAME B SAME
+    END
