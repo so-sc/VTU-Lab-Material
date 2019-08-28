35c841c23230f56116344b230282d6f798381c5c
Swasthik shetty <42874695+swaaz@users.noreply.github.com>
2019-08-29 00:14:51 +0530

    Update 05a-Evaluate a Postfix Expression.c

--- sem-3/computer_science/DS_LAB/05a-Evaluate a Postfix Expression/05a-Evaluate a Postfix Expression.c
+++ sem-3/computer_science/DS_LAB/05a-Evaluate a Postfix Expression/05a-Evaluate a Postfix Expression.c
@@ -1,83 +1,81 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<math.h>
+#include<stdlib.h>
+#include<string.h>
 #define max 5
-char postfix[50];
-struct  stack
+
+char postfix[100];
+
+struct stack
 {
-    int top;
     int item[max];
+    int top;
 }s;
+
 void push(int value)
 {
-    if(s.top==(max-1)) printf("Overflow\n");
-    else s.item[++s.top]=value;
+    if(s.top==max-1) printf("overflow\n");
+    else
+    {
+        //printf("Enter the element to be added\n");
+        s.item[++s.top]=value;
+    }
 }
 
 int pop()
 {
-    if(s.top==-1)
-    { printf("Underflow\n");
-    exit(0);
-    }
-    return(s.item[s.top--]);
+    if(s.top==-1) printf("underflow\n");
+    else return(s.item[s.top--]);
 }
 
 int empty()
 {
-    if(s.top==-1) return 1;
+     if(s.top==-1) return 1;
     else return 0;
-
 }
-
-int operation(int a,int b,char c)
+int operation(int a,int b,char o)
 {
-    switch(c)
+    switch (o)
     {
     case '^':return(pow(a,b));
-    case '*':return(a*b);
-    case '%':return(a%b);
     case '/':return(a/b);
-    case '+':return (a+b);
+    case '%':return(a%b);
+    case '*':return(a*b);
+    case '+':return(a+b);
     case '-':return(a-b);
+
 
     }
 }
-
 int evaluate()
+{
+    int i,a,b,ans,value;
+    char symb;
+    for(i=0;postfix[i]!='\0';i++)
     {
-        int i,a,b,ans,value;
-        char symb;
-        for(i=0;postfix[i]!='\0';i++)
+        symb=postfix[i];
+        if(symb>='0' && symb<='9')
         {
-            symb=postfix[i];
-            if((symb>='0') && (symb<='9'))
-            {
-                push((int) (symb-'0'));
-
-            }
-            else
-            {
-                a=pop();
-                b=pop();
-                value=operation(b,a,symb);
-                push(value);
-            }
-
+            push((int) (symb-'0'));
         }
-        ans =pop();
-        return ans;
+        else
+        {
+            a=pop();
+            b=pop();
+            value=operation(b,a,symb);
+            push(value);
+        }
 
     }
-
-    void main()
-    {
-        s.top=-1;
-        int ans;
-        printf("Enter the postfix expression \n");
-        gets(postfix);
-        ans=evaluate();
-        printf("The result ans = %d\n",ans);
-
-    }
-
+    ans=pop();
+    return ans;
+}
+void main()
+{
+    s.top=-1;
+    int ans;
+    printf("Enter the postfix expression\n");
+    gets(postfix);
+    ans=evaluate();
+    printf("Resultant=%d",ans);
+}
